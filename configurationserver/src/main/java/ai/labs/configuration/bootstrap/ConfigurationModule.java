ae66d08fb49a70f66534dcb6606dc2b9fb8a1e02
Gregor Jarisch <gregor@jarisch.net>
2017-02-05 17:42:40 +0100

    repaired maven pom files ; clean up

--- configurationserver/src/main/java/ai/labs/configuration/bootstrap/ConfigurationModule.java
+++ configurationserver/src/main/java/ai/labs/configuration/bootstrap/ConfigurationModule.java
@@ -2,8 +2,6 @@ package ai.labs.configuration.bootstrap;
 
 import ai.labs.permission.ssl.SelfSignedTrustProvider;
 import ai.labs.runtime.bootstrap.AbstractBaseModule;
-import ai.labs.staticresources.rest.IRestEditor;
-import ai.labs.staticresources.rest.impl.RestEditor;
 
 /**
  * @author ginccc
@@ -12,7 +10,5 @@ public class ConfigurationModule extends AbstractBaseModule {
     @Override
     protected void configure() {
         SelfSignedTrustProvider.setAlwaysTrust(true);
-
-        bind(IRestEditor.class).to(RestEditor.class);
     }
 }
