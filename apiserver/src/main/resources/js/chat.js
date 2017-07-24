f0c5f1cbb8ee6ab236930a85b3955973edfc6c14
Gregor Jarisch <gregor@edtechfoundry.com>
2017-07-24 10:57:39 +0200

    DIF-1345: improvements in JS UI

--- apiserver/src/main/resources/js/chat.js
+++ apiserver/src/main/resources/js/chat.js
@@ -1,95 +1,93 @@
-var eddi = {};
+const eddi = {};
 
-var Message;
-Message = function (arg) {
-    this.text = arg.text;
-    this.message_side = arg.message_side;
-    this.draw = function (_this) {
+class Message {
+    constructor(text, message_side) {
+        this.text = text;
+        this.message_side = message_side;
+    }
+
+    get draw() {
         return function () {
-            var $message;
-            $message = $($('.message_template').clone().html());
-            $message.addClass(_this.message_side).find('.text').html(_this.text);
+            const $message = $($('.message_template').clone().html());
+            $message.addClass(this.message_side).find('.text').html(this.text);
             $('.messages').append($message);
             return setTimeout(function () {
                 return $message.addClass('appeared');
             }, 0);
         };
-    }(this);
-    return this;
-};
-var QuickReply;
-QuickReply = function (arg) {
-    this.text = arg.text;
-    this.message_side = arg.message_side;
-    this.draw = function (_this) {
+    }
+}
+
+class QuickReply {
+    constructor(text, message_side) {
+        this.text = text;
+        this.message_side = message_side;
+    }
+
+    get draw() {
         return function () {
-            var $quickReply;
-            $quickReply = $('<button/>', {
-                text: _this.text,
+            const _this = this;
+            const $quickReply = $('<button/>', {
+                text: this.text,
                 click: function () {
                     eddi.submitUserMessage(_this.text);
                     eddi.displayMessage(_this.text, 'right');
                     $('.quick_reply').remove();
                 }
             });
-            $quickReply.addClass(_this.message_side);
+            $quickReply.addClass(this.message_side);
             $quickReply.addClass('quick_reply');
             $('.messages').append($quickReply);
             return setTimeout(function () {
                 return $quickReply.addClass('appeared');
             }, 0);
         };
-    }(this);
-    return this;
-};
+    }
+}
 
 $(function () {
-    var getMessageText, message_side, displayQuickReplies;
-    message_side = 'right';
-    getMessageText = function () {
-        var $message_input;
-        $message_input = $('.message_input');
+    let message_side = 'right';
+
+    function getMessageText() {
+        const $message_input = $('.message_input');
         return $message_input.val();
-    };
+    }
+
     eddi.displayMessage = function (text, side) {
-        var $messages, message;
+        let $messages, message;
         if (text.trim() === '') {
             return;
         }
         $('.message_input').val('');
         $messages = $('.messages');
         message_side = side ? 'right' : 'left';
-        message = new Message({
-            text: text,
-            message_side: side
-        });
+        message = new Message(text, side);
         message.draw();
         return $messages.animate({scrollTop: $messages.prop('scrollHeight')}, 300);
     };
-    displayQuickReplies = function (quickReplies) {
-        var $messages, quickReply;
+
+    function displayQuickReplies(quickReplies) {
+        let $messages, quickReply;
         if (quickReplies.length === 0) {
             return;
         }
 
         $messages = $('.messages');
-        for (var i = 0; i < quickReplies.length; i++) {
-            quickReply = new QuickReply({
-                text: quickReplies[i].value,
-                message_side: 'left'
-            });
+        for (let i = 0; i < quickReplies.length; i++) {
+            quickReply = new QuickReply(quickReplies[i].value, 'left');
             quickReply.draw();
         }
         return $messages.animate({scrollTop: $messages.prop('scrollHeight')}, 300);
-    };
-    $('.send_message').click(function (e) {
-        var userMessage = getMessageText();
+    }
+
+    $('.send_message').click(function () {
+        const userMessage = getMessageText();
         eddi.submitUserMessage(userMessage);
         return eddi.displayMessage(userMessage, 'right');
     });
     $('.message_input').keyup(function (e) {
         if (e.which === 13) {
-            var userMessage = getMessageText();
+            const userMessage = getMessageText();
             eddi.submitUserMessage(userMessage);
             return eddi.displayMessage(userMessage, 'right');
         }
@@ -107,58 +105,36 @@ $(function () {
         });
     };
 
-    var loadConversationLog = function () {
+    const loadConversationLog = function () {
         $.get("/bots/" + eddi.environment + "/" + eddi.botId + "/" + eddi.conversationId).done(function (data) {
             refreshConversationLog(data);
         });
     };
 
-    var refreshConversationLog = function (conversationMemory) {
-        var conversationState = conversationMemory.conversationState;
+    const refreshConversationLog = function (conversationMemory) {
+        const conversationState = conversationMemory.conversationState;
 
         if (conversationState === 'ERROR') {
             log('ERROR', "An Error has occurred. Please contact the administrator!");
             return;
         }
 
-        /*if (conversationState === 'ENDED') {
-            $("#ended_dialog").dialog({
-                modal: true,
-                buttons: [
-                    {
-                        text: "New Conversation!",
-                        click: function () {
-                            $(this).dialog("close");
-                            startNewConversation();
-                        }
-                    }
-                ],
-                position: {my: "top"},
-                width: 300
-            });
-            $("#ended_dialog").dialog("option", "closeOnEscape", false);
-            $('#status_indicator').css('visibility', 'hidden');
-            $('#user_input_button_submit').addClass('ui-disabled');
-            $('#redo').hide();
-            $('#undo').hide();
-        }*/
-
         if (conversationState === 'IN_PROGRESS') {
             setTimeout(loadConversationLog, 1000);
             return;
         }
 
-        var ioList = [];
-        for (var i = 0; i < conversationMemory.conversationSteps.length; i++) {
-            var step = conversationMemory.conversationSteps[i];
+        let ioList = [];
+        for (let i = 0; i < conversationMemory.conversationSteps.length; i++) {
+            let step = conversationMemory.conversationSteps[i];
 
-            var input = null;
-            var output = null;
-            var media = null;
-            var action = null;
-            var quickReplies = [];
-            for (var x = 0; x < step.data.length; x++) {
-                var obj = step.data[x];
+            let input = null;
+            let output = null;
+            let media = null;
+            let action = null;
+            let quickReplies = [];
+            for (let x = 0; x < step.data.length; x++) {
+                let obj = step.data[x];
                 if (obj.key.indexOf('input') === 0) {
                     input = obj.value;
                 } else if (obj.key.indexOf('media') === 0) {
@@ -176,39 +152,14 @@ $(function () {
         }
 
         if (ioList.length > 0) {
-            var latestInteraction = ioList[ioList.length - 1];
+            let latestInteraction = ioList[ioList.length - 1];
             if (latestInteraction === 'undefined') {
                 latestInteraction = {};
             }
 
-            //apply media/images
-            //var mediaURI = latestInteraction.media;
-            /*if (typeof mediaURI !== 'undefined' && mediaURI.indexOf('image') == 0) {
-             var mediaURL = baseURL + '/binary/default/mobile/images' + mediaURI.substring(mediaURI.lastIndexOf('/'));
-             var img = $('<img id="' + mediaURI.substring(mediaURI.lastIndexOf('/' + 1, mediaURI.lastIndexOf('.'))) + '">');
-             img.attr('src', mediaURL);
-             img.attr('class', 'media_img');
-             $('#desktop').html(img);
-             } else {
-             $('#desktop').html('<div style="text-align: center;width: 100%; height: 100%;">Nothing to show at the moment..</div>');
-             }*/
-
             if (latestInteraction.output === null) {
                 latestInteraction.output = '';
             }
-
-            /*var mediaURIsString = latestInteraction.media;
-             if (mediaURIsString != null) {
-             var mediaURIs = mediaURIsString.split(',');
-             for (var n = 0; n < mediaURIs.length; n++) {
-             var mediaURI = mediaURIs[n];
-             if (mediaURI.indexOf('pdf') == 0) {
-             var fileName = mediaURI.substring(mediaURI.lastIndexOf('/') + 1);
-             var mediaURL = baseURL + '/binary/default/desktop/binary/' + fileName;
-             latestInteraction.output += '<br /><a target="_blank" href="' + mediaURL + '">' + fileName + '</a>'
-             }
-             }
-             }*/
 
             eddi.displayMessage(latestInteraction.output, 'left');
             displayQuickReplies(latestInteraction.quickReplies);
@@ -217,17 +168,17 @@ $(function () {
         }
     };
 
-    var pushArray = function (arr, arr2) {
+    const pushArray = function (arr, arr2) {
         arr.push.apply(arr, arr2);
     };
 
-    var deployBot = function (environment, botId, botVersion) {
+    const deployBot = function (environment, botId, botVersion) {
         $.post("/administration/" + environment + "/deploy/" + botId + "?version=" + botVersion).done(function () {
             checkBotDeploymentStatus();
         });
     };
 
-    var checkBotDeploymentStatus = function () {
+    const checkBotDeploymentStatus = function () {
         $.get("/administration/" + eddi.environment + "/deploymentstatus/" + eddi.botId + "?version=" + eddi.botVersion).done(function (data) {
             if (data === 'IN_PROGRESS') {
                 setTimeout(checkBotDeploymentStatus, 1000);
@@ -239,16 +190,16 @@ $(function () {
         });
     };
 
-    var createConversation = function (environment, botId) {
+    const createConversation = function (environment, botId) {
         $.post("/bots/" + environment + "/" + botId).done(function (data, status, xhr) {
-            var conversationUriArray = xhr.getResponseHeader('Location').split("/");
+            const conversationUriArray = xhr.getResponseHeader('Location').split("/");
             eddi.conversationId = conversationUriArray[conversationUriArray.length - 1];
             proceedConversation();
         });
     };
 
-    var checkConversationStatus = function (environment, botId, conversationId) {
-        $.get("/bots/" + environment + "/" + botId + "/" + conversationId).always(function (data, status, xhr) {
+    const checkConversationStatus = function (environment, botId, conversationId) {
+        $.get("/bots/" + environment + "/" + botId + "/" + conversationId).always(function (data, status) {
             if (status === "error") {
                 alert("Checking conversation has yield into an error.. ");
             } else if (status === "success") {
@@ -262,16 +213,16 @@ $(function () {
         });
     };
 
-    var getQueryParts = function (href) {
-        var query = $.url.parse(href);
-        var path = query.path;
+    const getQueryParts = function (href) {
+        const query = $.url.parse(href);
+        const path = query.path;
 
-        var parts = path.split("/");
+        const parts = path.split("/");
 
-        var environment = null;
-        var botId = null;
-        var conversationId = null;
-        var botVersion = null;
+        let environment = null;
+        let botId = null;
+        let conversationId = null;
+        let botVersion = null;
 
         environment = typeof parts[2] !== 'undefined' ? decodeURIComponent(parts[2]) : environment;
         botId = typeof parts[3] !== 'undefined' ? decodeURIComponent(parts[3]) : botId;
@@ -283,7 +234,7 @@ $(function () {
         return {conversationId: conversationId, environment: environment, botId: botId, botVersion: botVersion};
     };
 
-    var proceedConversation = function () {
+    const proceedConversation = function () {
         if (!eddi.conversationId) {
             createConversation(eddi.environment, eddi.botId);
         } else {
@@ -291,7 +242,7 @@ $(function () {
         }
     };
 
-    var checkBotDeployment = function () {
+    const checkBotDeployment = function () {
         //check if bot is deployed
         $.get("/administration/" + eddi.environment + "/deploymentstatus/" + eddi.botId + "?version=" + eddi.botVersion)
             .done(function (data) {
@@ -316,7 +267,7 @@ $(function () {
     };
 
     $(document).ready(function () {
-        var extractedParams = getQueryParts(window.location.href);
+        const extractedParams = getQueryParts(window.location.href);
         //extract environment from URL
         if (extractedParams.environment !== null) {
             eddi.environment = extractedParams.environment;
@@ -343,12 +294,4 @@ $(function () {
             });
         }
     });
-
-    /*displayMessage('Hello Philip! :)', 'left');
-     setTimeout(function () {
-     return displayMessage('Hi Sandy! How are you?', 'right');
-     }, 1000);
-     return setTimeout(function () {
-     return displayMessage('I\'m fine, thank you!', 'left');
-     }, 2000);*/
 });
