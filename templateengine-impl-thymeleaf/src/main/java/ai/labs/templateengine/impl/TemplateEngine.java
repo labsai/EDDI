c250de0bb7c955ae018e16fa4609234cb1e943a2
Gregor Jarisch <gregor@edtechfoundry.com>
2017-07-31 15:15:07 +0200

    added first version of template engine

---
+++ templateengine-impl-thymeleaf/src/main/java/ai/labs/templateengine/impl/TemplateEngine.java
@@ -0,0 +1,21 @@
+package ai.labs.templateengine.impl;
+
+import ai.labs.templateengine.ITemplateEngine;
+
+import java.util.Map;
+
+/**
+ * @author ginccc
+ */
+public class TemplateEngine implements ITemplateEngine {
+
+
+    @Override
+    public String processTemplate(String template, Map<String, Object> models) {
+
+
+        return null;
+    }
+
+
+}
