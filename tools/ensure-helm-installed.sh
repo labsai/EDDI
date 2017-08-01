7fcd4bc0c153110b6b7ec2f7dc2eff8055409d51
Are Almaas <arealmaas@gmail.com>
2017-08-01 11:04:22 +0200

    use the new ci build

--- tools/ensure-helm-installed.sh
+++ tools/ensure-helm-installed.sh
@@ -5,6 +5,6 @@ set -x
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
-sudo helm init
-sudo helm repo add differ-charts https://differ-charts.storage.googleapis.com
-sudo helm repo update
+helm init
+helm repo add differ-charts https://differ-charts.storage.googleapis.com
+helm repo update
