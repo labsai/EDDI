37fbcac4b32c38a5e16e432474f7749dc3502878
Gregor Jarisch <gregor@edtechfoundry.com>
2017-08-02 13:13:14 +0200

    added further unit tests

--- templateengine-impl-thymeleaf/src/test/java/ai/labs/templateengine/impl/TemplatingEngineTest.java
+++ templateengine-impl-thymeleaf/src/test/java/ai/labs/templateengine/impl/TemplatingEngineTest.java
@@ -2,6 +2,9 @@ package ai.labs.templateengine.impl;
 
 import ai.labs.templateengine.ITemplatingEngine;
 import ai.labs.templateengine.bootstrap.TemplateEngineModule;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,7 +24,7 @@ public class TemplatingEngineTest {
     }
 
     @Test
-    public void processTemplate() throws Exception {
+    public void processTemplateWithStringContext() throws Exception {
         //setup
         String template = "Some kind of string having a [[${value}]]";
         HashMap<String, Object> dynamicAttributesMap = new HashMap<>();
@@ -32,5 +35,26 @@ public class TemplatingEngineTest {
 
         //assert
         Assert.assertEquals("Some kind of string having a testValue", result);
+    }
+
+    @Test
+    public void processTemplateWithObjectContext() throws Exception {
+        //setup
+        String template = "Some kind of string having a [[${obj.value}]]";
+        HashMap<String, Object> dynamicAttributesMap = new HashMap<>();
+        dynamicAttributesMap.put("obj", new TestObject("testValue"));
+
+        //test
+        String result = templatingEngine.processTemplate(template, dynamicAttributesMap);
+
+        //assert
+        Assert.assertEquals("Some kind of string having a testValue", result);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    private static class TestObject {
+        private String value;
     }
 }
