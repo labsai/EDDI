ba50ea5af9a22f180766267eb82ee70bd0bd3820
Gregor Jarisch <gregor@edtechfoundry.com>
2017-08-07 11:25:33 +0200

    updated circle ci config file

--- tools/ensure-helm-installed.sh
+++ tools/ensure-helm-installed.sh
@@ -5,7 +5,7 @@ set -x
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
-helm repo add differ-charts https://differ-charts.storage.googleapis.com
 helm repo remove local
 helm init
+helm repo add differ-charts https://differ-charts.storage.googleapis.com
 helm repo update
