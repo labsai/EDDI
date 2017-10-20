911b7eab60d360d76c54b00cc69e20ffd8cf073f
Gregor Jarisch <gregor@jarisch.net>
2017-10-20 15:36:09 +0200

    removed unsafe ssl acceptance in http client

--- corerest-definition/src/main/java/ai/labs/rest/restinterfaces/RestInterfaceFactory.java
+++ corerest-definition/src/main/java/ai/labs/rest/restinterfaces/RestInterfaceFactory.java
@@ -2,12 +2,6 @@ package ai.labs.rest.restinterfaces;
 
 import ai.labs.runtime.ThreadContext;
 import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.conn.ssl.X509HostnameVerifier;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
@@ -15,12 +9,7 @@ import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.net.ssl.*;
 import javax.ws.rs.client.ClientRequestFilter;
-import java.io.IOException;
-import java.net.URI;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -60,8 +49,6 @@ public class RestInterfaceFactory implements IRestInterfaceFactory {
     private ResteasyClient getResteasyClient(String targetServerUri) throws RestInterfaceFactoryException {
         ResteasyClient client = clients.get(targetServerUri);
         if(client == null) {
-            HttpClient httpClient = targetServerUri.startsWith("https") ?
-                    prepareClientForSSL(this.httpClient, URI.create(targetServerUri)) : this.httpClient;
 
             ApacheHttpClient4Engine engine = new ApacheHttpClient4Engine(httpClient);
             ResteasyClientBuilder clientBuilder = new ResteasyClientBuilder();
@@ -72,52 +59,6 @@ public class RestInterfaceFactory implements IRestInterfaceFactory {
         }
 
         return client;
-    }
-
-    private HttpClient prepareClientForSSL(HttpClient base, URI baseURI) throws RestInterfaceFactoryException {
-        try {
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            X509TrustManager tm = new X509TrustManager() {
-
-                public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-                }
-
-                public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-                }
-
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-            };
-            X509HostnameVerifier verifier = new X509HostnameVerifier() {
-
-                @Override
-                public void verify(String string, SSLSocket ssls) throws IOException {
-                }
-
-                @Override
-                public void verify(String string, X509Certificate xc) throws SSLException {
-                }
-
-                @Override
-                public void verify(String string, String[] strings, String[] strings1) throws SSLException {
-                }
-
-                @Override
-                public boolean verify(String string, SSLSession ssls) {
-                    return true;
-                }
-            };
-            sslContext.init(null, new TrustManager[]{tm}, null);
-            SSLSocketFactory sslSocketFactory = new SSLSocketFactory(sslContext);
-            sslSocketFactory.setHostnameVerifier(verifier);
-            ClientConnectionManager ccm = base.getConnectionManager();
-            SchemeRegistry schemeRegistry = ccm.getSchemeRegistry();
-            schemeRegistry.register(new Scheme(baseURI.getScheme(), baseURI.getPort(), sslSocketFactory));
-            return new DefaultHttpClient(ccm, base.getParams());
-        } catch (Exception ex) {
-            throw new RestInterfaceFactoryException("Error while preparing client connection for server.", ex);
-        }
     }
 
     public static class RestInterfaceFactoryException extends Exception {
