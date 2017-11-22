49028e957b1e1d2a5f3c6e5222fff91973562ed2
Gregor Jarisch <gregor@jarisch.net>
2017-11-22 10:52:38 +0100

    added swagger annotations to backup services

--- backupservice-defintion/src/main/java/ai/labs/backupservice/IRestExportService.java
+++ backupservice-defintion/src/main/java/ai/labs/backupservice/IRestExportService.java
@@ -1,19 +1,33 @@
 package ai.labs.backupservice;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ResponseHeader;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.net.URI;
 
 /**
  * @author ginccc
  */
+@Api(value = "backup")
 @Path("/backup/export")
 public interface IRestExportService {
     @GET
     @Produces("application/zip")
     @Path("{botFilename}")
+    @ApiResponse(code = 200, response = InputStream.class, message = "returns bot zip file as application/zip")
     Response getBotZipArchive(@PathParam("botFilename") String botFilename);
 
     @POST
     @Path("{botId}")
-    Response exportBot(@PathParam("botId") String botId, @QueryParam("botVersion") @DefaultValue("1") Integer botVersion);
+    @ApiResponse(code = 200, responseHeaders = {
+            @ResponseHeader(name = "location", response = URI.class)
+    }, message = "returns location of the exported ZIP file to be downloaded")
+    Response exportBot(@PathParam("botId") String botId,
+                       @ApiParam(name = "botVersion", required = true, format = "integer", example = "1")
+                       @QueryParam("botVersion") @DefaultValue("1") Integer botVersion);
 }
--- backupservice-defintion/src/main/java/ai/labs/backupservice/IRestImportService.java
+++ backupservice-defintion/src/main/java/ai/labs/backupservice/IRestImportService.java
@@ -1,18 +1,29 @@
 package ai.labs.backupservice;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ResponseHeader;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 import java.io.InputStream;
+import java.net.URI;
 
 /**
  * @author ginccc
  */
+@Api(value = "backup")
 @Path("backup/import")
 public interface IRestImportService {
     @POST
     @Consumes("application/zip")
+    @ApiImplicitParam(paramType = "body", required = true)
+    @ApiResponse(code = 200, responseHeaders = {
+            @ResponseHeader(name = "location", response = URI.class)
+    }, message = "returns reference to the newly created (imported) bot")
     void importBot(InputStream zippedBotConfigFiles, @Suspended AsyncResponse response);
 }
