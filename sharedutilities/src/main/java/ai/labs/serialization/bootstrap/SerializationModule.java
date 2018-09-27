0fe697d48e0faa06db82a4760d0bf5e4488539a2
Gregor Jarisch <gregor@jarisch.net>
2018-09-27 16:22:08 +0200

    updated infinispan; updated jetty; updated log4j; updated google guice; updated docker file (using java11 now); added better error handling for parsing expressions; some refactorings around executorService

--- sharedutilities/src/main/java/ai/labs/serialization/bootstrap/SerializationModule.java
+++ sharedutilities/src/main/java/ai/labs/serialization/bootstrap/SerializationModule.java
@@ -28,11 +28,9 @@ public class SerializationModule extends AbstractModule {
     public ObjectMapper provideObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
-
 }
--- xmpp-impl/src/main/java/ai/labs/xmpp/bootstrap/XmppModule.java
+++ xmpp-impl/src/main/java/ai/labs/xmpp/bootstrap/XmppModule.java
@@ -1,23 +1,17 @@
 package ai.labs.xmpp.bootstrap;
 
-import ai.labs.xmpp.endpoint.IXmppEndpoint;
 import ai.labs.runtime.bootstrap.AbstractBaseModule;
+import ai.labs.xmpp.endpoint.IXmppEndpoint;
 import ai.labs.xmpp.endpoint.XmppEndpoint;
-import com.google.inject.Scopes;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author rpi
  */
-@Slf4j
+
 public class XmppModule extends AbstractBaseModule {
-
-
 
     @Override
     protected void configure() {
-        log.info("XMPP configure");
         bind(IXmppEndpoint.class).to(XmppEndpoint.class);
-
     }
 }
