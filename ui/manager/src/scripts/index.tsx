55eb67d5479121f2e3bbc882c089090cc64550ab
DESKTOP-Jonas <jonas_horvli@hotmail.com>
2018-10-07 19:07:23 +0200

    Reset, and attempting husky fix.

--- ui/manager/src/scripts/index.tsx
+++ ui/manager/src/scripts/index.tsx
@@ -4,10 +4,8 @@ import { render } from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
 import { store } from './store/store';
-
-export const history = createBrowserHistory();
+import { history } from './history';
 
 render(
   <Provider store={store}>
--- ui/manager/src/tests/unit/tests/smoke.spec.ts
+++ ui/manager/src/tests/unit/tests/smoke.spec.ts
@@ -1,4 +1,4 @@
-import {assert} from 'chai';
+import { assert } from 'chai';
 
 describe('Smoke test', function() {
   it('should support async functions', async function() {
