bcee6677650b687f0bd3f3f619addc0ee44044b6
rpi <pickl@dolph.in>
2019-02-05 11:37:53 +0100

    some apis return json, with wrong mime type

--- httpCallsTask-impl/src/main/java/ai/labs/restapi/connector/impl/HttpCallsTask.java
+++ httpCallsTask-impl/src/main/java/ai/labs/restapi/connector/impl/HttpCallsTask.java
@@ -147,9 +147,9 @@ public class HttpCallsTask implements ILifecycleTask {
                             }
 
                             if (!CONTENT_TYPE_APPLICATION_JSON.startsWith(actualContentType)) {
-                                String message = "HttpCall (%s) didn't return application/json as content-type, instead was (%s)";
+                                String message = "HttpCall (%s) didn't return application/json as content-type, instead was (%s) - still trying json";
                                 log.warn(String.format(message, call.getName(), actualContentType));
-                                continue;
+                                //continue;
                             }
 
                             Object responseObject = jsonSerialization.deserialize(responseBody, Object.class);
