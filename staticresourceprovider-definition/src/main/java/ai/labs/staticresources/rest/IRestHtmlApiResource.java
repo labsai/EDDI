d9b53f5065fc9c6a31e79c32371cc02a9d7dd04f
Kasper <kasper.kadzielawa@gmail.com>
2019-03-25 18:48:14 +0100

    Add `Produces` annotation to IRestHtmlChatResource

--- staticresourceprovider-definition/src/main/java/ai/labs/staticresources/rest/IRestHtmlApiResource.java
+++ staticresourceprovider-definition/src/main/java/ai/labs/staticresources/rest/IRestHtmlApiResource.java
@@ -3,12 +3,14 @@ package ai.labs.staticresources.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  * @author ginccc
  */
 
 @Path("/view")
+@Produces("text/html")
 public interface IRestHtmlApiResource {
 
     @GET
