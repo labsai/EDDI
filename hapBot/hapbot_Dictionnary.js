57acf312a84a67dc040e4d102f7a9ed35c4d1a8b
Eunit <eunitwap@gmail.com>
2019-05-31 15:12:40 +0100

    Create hapbot_Dictionnary.js

---
+++ hapBot/hapbot_Dictionnary.js
@@ -0,0 +1,485 @@
+{
+	"words": [
+		{
+			"word": "hi",
+			"exp": "greeting(hi)",
+			"frequency": 0
+		},
+		{
+			"word": "hello",
+			"exp": "greeting(hello)",
+			"frequency": 0
+		},
+		{
+			"word": "yes",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "cool",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "no",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "uninterested",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "yea",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "nope",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "yeah",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "nah",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "nay",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "yo",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "none",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "n",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "ne",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "y",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "ye",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "ya",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "yep",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "na",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "not",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "sure",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "don't",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "uhmm",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "sad",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "notatall",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "decline",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "bad",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "ofcourse",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "definitely",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "probably",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "happy",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "more",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "thanks",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "thank",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "again",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "noway",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "aye",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "some",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "nothing",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "absolutely",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "never",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "okeydoke",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "okeydokey",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "tada",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "nice",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "con",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "positive",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "maybe",
+			"exp": "confirmation(yes)",
+			"frequency": 0
+		},
+		{
+			"word": "negative",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "yuck",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "stink",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "ill",
+			"exp": "confirmation(no)",
+			"frequency": 0
+		},
+		{
+			"word": "next",
+			"exp": "nexttip",
+			"frequency": 0
+		},
+		{
+			"word": "end",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "end",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "bye",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "goodbye",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "stop",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "later",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "tired",
+			"exp": "ending",
+			"frequency": 0
+		},
+		{
+			"word": "fuck",
+			"exp": "insult(vulgar)",
+			"frequency": 0
+		},
+		{
+			"word": "shit",
+			"exp": "insult(vulgar)",
+			"frequency": 0
+		},
+		{
+			"word": "cunt",
+			"exp": "insult(vulgar)",
+			"frequency": 0
+		},
+		{
+			"word": "whore",
+			"exp": "insult(vulgar)",
+			"frequency": 0
+		},
+		{
+			"word": "dick",
+			"exp": "insult(vulgar)",
+			"frequency": 0
+		},
+		{
+			"word": "pussy",
+			"exp": "insult(vulgar)",
+			"frequency": 0
+		}
+	],
+	"phrases": [
+		{
+			"phrase": "good morning",
+			"exp": "greeting(good_morning)"
+		},
+		{
+			"phrase": "good evening",
+			"exp": "greeting(good_evening)"
+		},
+		{
+			"phrase": "good night",
+			"exp": "greeting(good_night)"
+		},
+		{
+			"phrase": "how are you",
+			"exp": "how_are_you"
+		},
+		{
+			"phrase": "how are you doing",
+			"exp": "how_are_you_doing"
+		},
+		{
+			"phrase": "my name is",
+			"exp": "name"
+		},
+		{
+			"phrase": "i am supposed",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "i am not supposed",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "i'm supposed",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "shut up",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "i'm not supposed",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "of course",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "sounds nice",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "hell no",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "go to hell",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "shut up",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "sounds convincing",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "sounds good",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "will be nice",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "good idea",
+			"exp": "confirmation(yes)"
+		},
+		{
+			"phrase": "not a good idea",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "not interested",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "not at all",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "hell no",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "go to hell",
+			"exp": "confirmation(no)"
+		},
+		{
+			"phrase": "i am",
+			"exp": "name"
+		},
+		{
+			"phrase": "fuck off",
+			"exp": "insult(vulgar)"
+		},
+		{
+			"phrase": "fuck of",
+			"exp": "insult(vulgar)"
+		},
+		{
+			"phrase": "fuck you",
+			"exp": "insult(vulgar)"
+		},
+		{
+			"phrase": "fuck ya",
+			"exp": "insult(vulgar)"
+		},
+		{
+			"phrase": "see you",
+			"exp": "ending"
+		},
+		{
+			"phrase": "got to go",
+			"exp": "ending"
+		},
+		{
+			"phrase": "good bye",
+			"exp": "ending"
+		},
+		{
+			"phrase": "next time",
+			"exp": "ending"
+		},
+		{
+			"phrase": "thanks anyway",
+			"exp": "ending"
+		},
+		{
+			"phrase": "nice day",
+			"exp": "ending"
+		}
+	]
+}
