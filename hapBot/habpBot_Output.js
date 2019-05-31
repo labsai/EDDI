01ea296507a3f23df778d6fc84deb586196b7098
Eunit <eunitwap@gmail.com>
2019-05-31 15:23:11 +0100

    Create habpBot_Output.js

---
+++ hapBot/habpBot_Output.js
@@ -0,0 +1,528 @@
+{
+	"outputSet": [
+		{
+			"action": "ending",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"I'm glad you made it up to this phase, I wish everything works together to make you more happier."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Before I go, I want you to know this: 'Happiness is like a butterfly: the more you chase it, the more it will elude you, but if you turn your attention to other things, it will come and sit softly on your shoulder.' - Henry David Thoreau"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"See ya! 👋"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Sources of these tips include: <b>WebMD</b>, <b>Psychology Today</b>. hapBot was developed by <b>Eunit99</b> for LabsAI. © 2019. All Rights Reserved."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "ending_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" I'm glad you made it up to this phase, I wish everything works together to make you more happier."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Before I go, I want you to know this: 'Happiness is like a butterfly: the more you chase it, the more it will elude you, but if you turn your attention to other things, it will come and sit softly on your shoulder.' - Henry David Thoreau"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"See ya! 👋"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Sources of these tips include: <b>WebMD</b>, <b>Psychology Today</b>. hapBot was developed by <b>Eunit99</b> for LabsAI. © 2019. All Rights Reserved."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "first_tip_no",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"You deserve to be happy! So free up your mind."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Try remembering reasons for which you should be happy."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Your handsome husband / beautiful wife, your children, you being alive now, your ability to read this! 😇"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Ain't you supposed to be happy?"
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "first_tip_no_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" you deserve to be happy! So free up your mind."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Try remembering reasons for which you should be happy."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Your handsome husband / beautiful wife, your children, you being alive now, your ability to read this! 😇"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Ain't you supposed to be happy?"
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "first_tip_yes",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Try remembering reasons for which you should be happy."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Your handsome husband / beautiful wife, your children, you being alive now, your ability to read this! 😇"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Ain't you supposed to be happy?"
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "first_tip_yes_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" try remembering reasons for which you should be happy."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Your handsome husband / beautiful wife, your children, you being alive now, your ability to read this! 😇"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Ain't you supposed to be happy?"
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_fifth",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 6: Foster Friendship"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"There are few better antidotes to unhappiness than close friendships with people who care about you, says David G. Myers, author of The Pursuit of Happiness. One Australian study found that people over 70 who had the strongest network of friends lived much longer."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_fifth_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 6: Foster Friendship"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"There are few better antidotes to unhappiness than close friendships with people who care about you, says David G. Myers, author of The Pursuit of Happiness. One Australian study found that people over 70 who had the strongest network of friends lived much longer."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_first",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 2: Cultivate gratitude"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"In his book, Authentic Happiness, University of Pennsylvania psychologist Martin Seligman encourages readers to perform a daily 'gratitude exercise'. It involves listing a few things that make them grateful. This shifts people away from bitterness and despair, he says, and promotes happiness."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_first_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 2: Cultivate gratitude"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"In his book, Authentic Happiness, University of Pennsylvania psychologist Martin Seligman encourages readers to perform a daily 'gratitude exercise'. It involves listing a few things that make them grateful. This shifts people away from bitterness and despair, he says, and promotes happiness."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_fourth",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 5: Remember, Money Can't Buy Happiness"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Research shows that once income climbs above the poverty level, more money brings very little extra happiness. Yet, 'we keep assuming that because things are not bringing us happiness, they are the wrong things, rather than recognizing that the pursuit itself is futile,' writes Daniel Gilbert in his book, Stumbling on Happiness. 'Regardless of what we achieve in the pursuit of stuff, it is never going to bring about an enduring state of happiness.'"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_fourth_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 5: Remember, Money Can't Buy Happiness"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Research shows that once income climbs above the poverty level, more money brings very little extra happiness. Yet, 'we keep assuming that because things are not bringing us happiness, they are the wrong things, rather than recognizing that the pursuit itself is futile,' writes Daniel Gilbert in his book, Stumbling on Happiness. 'Regardless of what we achieve in the pursuit of stuff, it is never going to bring about an enduring state of happiness.'"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_second",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 3:  Foster Forgiveness"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"According to a research, holding a grudge and nursing grievances can affect physical as well as mental health. One way to curtail these kinds of feelings is to foster forgiveness. This reduces the power of bad events to create bitterness and resentment, say Michael McCullough and Robert Emmons, happiness researchers who edited The Psychology of Happiness."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_second_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 3:  Foster Forgiveness"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"According to a research, holding a grudge and nursing grievances can affect physical as well as mental health. One way to curtail these kinds of feelings is to foster forgiveness. This reduces the power of bad events to create bitterness and resentment, say Michael McCullough and Robert Emmons, happiness researchers who edited The Psychology of Happiness."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_seventh",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"Finally,"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"It turns out that happiness can be a matter of choice -- not just luck. Some people are lucky enough to possess genes that foster happiness. However, certain thought patterns and interpersonal skills definitely foster happiness in their lives."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"I've got a summary for you on 'How To Stay Happy', do you wish to check it out?"
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_seventh_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" finally,"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"It turns out that happiness can be a matter of choice -- not just luck. Some people are lucky enough to possess genes that foster happiness. However, certain thought patterns and interpersonal skills definitely foster happiness in their lives."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"I've got a summary for you on 'How To Stay Happy', do you wish to check it out?"
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_sixth",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 7:  Engage in Meaningful Activities"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"People are seldom happier. To get more out of life, we need to put more into it, says Csikszentmihalyi. 'Active leisure that helps a person grow does not come easily,' he writes in Finding Flow. 'Each of the flow-producing activities requires an initial investment of attention before it begins to be enjoyable.' "
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_sixth_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 7:  Engage in Meaningful Activities"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"People are seldom happier. To get more out of life, we need to put more into it, says Csikszentmihalyi. 'Active leisure that helps a person grow does not come easily,' he writes in Finding Flow. 'Each of the flow-producing activities requires an initial investment of attention before it begins to be enjoyable.' "
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_third",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 4: Counteract Negative Thoughts and Feelings"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"As Jon Haidt puts it, improve your mental hygiene. In The Happiness Hypothesis, Haidt compares the mind to a man riding an elephant. The elephant represents the powerful thoughts and feelings -- mostly unconscious -- that drive your behavior. The man, although much weaker, can exert control over the elephant, just as you can exert control over negative thoughts and feelings. 'The key is a commitment to doing the things necessary to retrain the elephant,' Haidt says. 'And the evidence suggests there is a lot you can do. It just takes work.' For example, you can practice meditation, rhythmic breathing, yoga, or relaxation techniques to quell anxiety and promote serenity. You can learn to recognize and challenge thoughts you have about being inadequate and helpless."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		},
+		{
+			"action": "happiness_strategy_third_fallback",
+			"timesOccurred": 0,
+			"outputs": [
+				{
+					"type": "text",
+					"valueAlternatives": [
+						" # 4: Counteract Negative Thoughts and Feelings"
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"As Jon Haidt puts it, improve your mental hygiene. In The Happiness Hypothesis, Haidt compares the mind to a man riding an elephant. The elephant represents the powerful thoughts and feelings -- mostly unconscious -- that drive your behavior. The man, although much weaker, can exert control over the elephant, just as you can exert control over negative thoughts and feelings. 'The key is a commitment to doing the things necessary to retrain the elephant,' Haidt says. 'And the evidence suggests there is a lot you can do. It just takes work.' For example, you can practice meditation, rhythmic breathing, yoga, or relaxation techniques to quell anxiety and promote serenity. You can learn to recognize and challenge thoughts you have about being inadequate and helpless."
+					]
+				},
+				{
+					"type": "text",
+					"valueAlternatives": [
+						"For next, simply type 'Next' ."
+					]
+				}
+			],
+			"quickReplies": []
+		}
+	]
+}
