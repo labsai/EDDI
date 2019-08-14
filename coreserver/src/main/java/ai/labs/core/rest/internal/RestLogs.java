6a82c6167134d8453932ef400df5494268f1d3ee
ginccc <gregor@jarisch.net>
2019-08-14 11:26:47 +0200

    added rest-endpoint in order to change Log Level in runtime for a specific packageName e.g. POST http://localhost:7070/logs?packageName=ai.labs.channels&logLevel=DEBUG

--- coreserver/src/main/java/ai/labs/core/rest/internal/RestLogs.java
+++ coreserver/src/main/java/ai/labs/core/rest/internal/RestLogs.java
@@ -3,18 +3,39 @@ package ai.labs.core.rest.internal;
 import ai.labs.models.DatabaseLog;
 import ai.labs.rest.rest.IRestLogs;
 import ai.labs.runtime.IDatabaseLogs;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.util.List;
 
 import static ai.labs.models.Deployment.Environment;
+import static ai.labs.utilities.RuntimeUtilities.checkNotNull;
 
+@Slf4j
 public class RestLogs implements IRestLogs {
     private final IDatabaseLogs databaseLogs;
 
     @Inject
     public RestLogs(IDatabaseLogs databaseLogs) {
         this.databaseLogs = databaseLogs;
+    }
+
+    @Override
+    public void setLogLevel(String packageName, String logLevel) {
+        checkNotNull(packageName, "packageName");
+        checkNotNull(logLevel, "logLevel");
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        Logger logger = loggerContext.getLogger(packageName);
+        log.info("Changing LogLevel for packageName: " + packageName + " with current logger level: " + logger.getLevel());
+        logger.setLevel(Level.toLevel(logLevel));
+        log.info("Changed LogLevel for packageName: " + packageName + " to logger level: " + logger.getLevel());
+
     }
 
     @Override
