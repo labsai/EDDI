a2f49a88bfcab7be58b89f7948b0d2bf7e67153f
rpi <pickl@dolph.in>
2020-09-26 16:18:57 +0200

    git calls lifecycle task stub

---
+++ gitCallsTask-impl/src/main/java/ai/labs/git/connector/impl/GitCallsTask.java
@@ -0,0 +1,22 @@
+package ai.labs.git.connector.impl;
+
+import ai.labs.lifecycle.ILifecycleTask;
+import ai.labs.lifecycle.LifecycleException;
+import ai.labs.memory.IConversationMemory;
+
+public class GitCallsTask implements ILifecycleTask {
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public Object getComponent() {
+        return null;
+    }
+
+    @Override
+    public void executeTask(IConversationMemory memory) throws LifecycleException {
+
+    }
+}
