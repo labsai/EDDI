87404c60592c203dc59c61413fe2fb967f9e71cd
ginccc <gregor@jarisch.net>
2021-01-14 10:00:14 +0100

    added fallback to properties for user language retrieval if user language is not handed over via context

--- conversationmemory-impl/src/main/java/ai/labs/memory/ContextUtilities.java
+++ conversationmemory-impl/src/main/java/ai/labs/memory/ContextUtilities.java
@@ -5,12 +5,15 @@ import ai.labs.models.Property;
 
 import java.util.List;
 
+import static ai.labs.utilities.RuntimeUtilities.isNullOrEmpty;
+
 public class ContextUtilities {
     private static final String KEY_LANG = "lang";
     private static final String KEY_CONTEXT = "context";
+    public static final String DEFAULT_USER_LANGUAGE = "en";
 
     public static String retrieveAndStoreContextLanguageInLongTermMemory(IConversationMemory memory) {
-        var lang = "en";
+        String lang = null;
         var currentStep = memory.getCurrentStep();
         List<IData<Context>> contextDataList = currentStep.getAllData(KEY_CONTEXT);
         for (IData<Context> contextData : contextDataList) {
@@ -21,6 +24,11 @@ public class ContextUtilities {
                 lang = context.getValue().toString();
                 memory.getConversationProperties().put(KEY_LANG, new Property(KEY_LANG, lang, Property.Scope.longTerm));
             }
+        }
+
+        if (isNullOrEmpty(lang)) {
+            Property languageProperty = memory.getConversationProperties().get(KEY_LANG);
+            lang = languageProperty != null ? languageProperty.getValue().toString() : DEFAULT_USER_LANGUAGE;
         }
 
         return lang;
