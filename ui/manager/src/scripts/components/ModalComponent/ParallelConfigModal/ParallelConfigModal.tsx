2bf201b64b677fc8872ab3a69ab65c90019c075a
stanislav.sidorov <stanislav.sidorov@empeek.io>
2021-08-11 11:22:15 +0300

    fix(editor): undo functionality improvements, disable swiping

--- ui/manager/src/scripts/components/ModalComponent/ParallelConfigModal/ParallelConfigModal.tsx
+++ ui/manager/src/scripts/components/ModalComponent/ParallelConfigModal/ParallelConfigModal.tsx
@@ -60,6 +60,7 @@ const ParallelConfigModal = ({ packageResource }: IPublicProps) => {
     adaptiveHeight: true,
     speed: 500,
     arrows: false,
+    swipe: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     afterChange: (index: number) => {
