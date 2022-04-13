6de1698ff3c0d69934a7b2317b5bcf63b3bc04fa
ginccc <gregor@jarisch.net>
2022-04-13 19:08:30 +0200

    Merged Startup and AutoBotDeployment classes

--- src/main/java/ai/labs/eddi/engine/runtime/internal/AutoBotDeployment.java
+++ src/main/java/ai/labs/eddi/engine/runtime/internal/AutoBotDeployment.java
@@ -7,21 +7,25 @@ import ai.labs.eddi.engine.runtime.IAutoBotDeployment;
 import ai.labs.eddi.engine.runtime.IBotFactory;
 import ai.labs.eddi.engine.runtime.service.ServiceException;
 import ai.labs.eddi.models.Deployment.Environment;
+import io.quarkus.runtime.Startup;
+import io.quarkus.runtime.StartupEvent;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 /**
  * @author ginccc
  */
 
+@Startup(4000)
 @ApplicationScoped
 public class AutoBotDeployment implements IAutoBotDeployment {
     private final IDeploymentStore deploymentStore;
     private final IBotFactory botFactory;
 
-    private static final Logger log = Logger.getLogger(AutoBotDeployment.class);
+    private static final Logger LOGGER = Logger.getLogger(AutoBotDeployment.class);
 
     @Inject
     public AutoBotDeployment(IDeploymentStore deploymentStore, IBotFactory botFactory) {
@@ -29,10 +33,18 @@ public class AutoBotDeployment implements IAutoBotDeployment {
         this.botFactory = botFactory;
     }
 
+    void onStart(@Observes StartupEvent ev) {
+        try {
+            autoDeployBots();
+        } catch (IAutoBotDeployment.AutoDeploymentException e) {
+            LOGGER.error(e.getLocalizedMessage(), e);
+        }
+    }
+
     @Override
     public void autoDeployBots() throws AutoDeploymentException {
         try {
-            log.info("Starting auto deployment of bots...");
+            LOGGER.info("Starting auto deployment of bots...");
             deploymentStore.readDeploymentInfos().stream().filter(
                             deploymentInfo -> deploymentInfo.getDeploymentStatus() == DeploymentStatus.deployed).
                     forEach(deploymentInfo -> {
@@ -43,11 +55,11 @@ public class AutoBotDeployment implements IAutoBotDeployment {
                             botFactory.deployBot(environment, botId, botVersion, null);
                         } catch (ServiceException | IllegalAccessException | IllegalArgumentException e) {
                             String message = "Error while auto deploying bot (environment=%s, botId=%s, version=%s)!\n";
-                            log.error(String.format(message, environment, botId, botVersion));
-                            log.error(e.getLocalizedMessage(), e);
+                            LOGGER.error(String.format(message, environment, botId, botVersion));
+                            LOGGER.error(e.getLocalizedMessage(), e);
                         }
                     });
-            log.info("Finished auto deployment of bots.");
+            LOGGER.info("Finished auto deployment of bots.");
         } catch (IResourceStore.ResourceStoreException e) {
             throw new AutoDeploymentException(e.getLocalizedMessage(), e);
         }
