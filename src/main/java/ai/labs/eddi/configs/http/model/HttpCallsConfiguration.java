2b5c480e0a0af2e8e79998e68be6dea79c888db4
ginccc <gregor@jarisch.net>
2022-06-01 23:10:14 +0200

    removed legacy property alias

--- src/main/java/ai/labs/eddi/configs/http/model/HttpCallsConfiguration.java
+++ src/main/java/ai/labs/eddi/configs/http/model/HttpCallsConfiguration.java
@@ -1,7 +1,6 @@
 package ai.labs.eddi.configs.http.model;
 
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +13,6 @@ import java.util.List;
 @Getter
 @Setter
 public class HttpCallsConfiguration {
-    @JsonAlias("targetServer")
     private String targetServerUrl;
     private List<HttpCall> httpCalls;
 }
