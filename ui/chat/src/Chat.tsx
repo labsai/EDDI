e07a9682abebb35db333b249d1b93ce2670de0b3
Gregor Jarisch <gregor@jarisch.net>
2024-02-12 18:23:07 +0100

    EDDI Chat UI for managedbots and bot engine api

---
+++ ui/chat/src/Chat.tsx
@@ -0,0 +1,254 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {useLocation, useParams} from "react-router-dom";
+
+type Message = {
+    sender: 'user' | 'bot';
+    text: string;
+};
+
+type QuickReply = {
+    value: string;
+};
+
+const Chat: React.FC = () => {
+    const [input, setInput] = useState<string>('');
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [quickReplies, setQuickReplies] = useState<QuickReply[]>([]);
+    const [isBotTyping, setIsBotTyping] = useState<boolean>(false);
+    const [hasNewMessages, setHasNewMessages] = useState<boolean>(false);
+    const [autoScroll, setAutoScroll] = useState<boolean>(true);
+    const [conversationId, setConversationId] = useState<string | null>(null);
+    const [isManagedBots, setIsManagedBots] = useState<boolean>(false);
+
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    const messagesContainerRef = useRef<HTMLDivElement>(null);
+
+    const {intent, environment, userId: userIdFromParams, botId} = useParams();
+    const location = useLocation();
+
+    const userId = userIdFromParams != null ? userIdFromParams : new URLSearchParams(location.search).get('userId');
+
+    const managedBotsBaseUrl = process.env.REACT_APP_MANAGED_BOTS_BASE_URL;
+    const botsBaseUrl = process.env.REACT_APP_BOTS_BASE_URL;
+
+    const startConversation = async () => {
+        try {
+            const response = await fetch(`${botsBaseUrl}/${environment}/${botId}?userId=${userId}`, {
+                method: 'POST',
+            });
+
+            if (response.ok) {
+                // Extract the Location header
+                const locationHeader = response.headers.get('Location');
+                console.log("Location Header:", locationHeader);
+
+                if (locationHeader) {
+                    // Assuming the conversationId is the last segment in the URI
+                    const segments = locationHeader.split('/');
+                    const conversationId = segments[segments.length - 1];
+                    setConversationId(conversationId);
+                    console.log("Extracted conversationId:", conversationId);
+                } else {
+                    throw new Error('Location header is missing in the response.');
+                }
+            }
+        } catch (error) {
+            console.error("Error starting conversation: ", error);
+        }
+    };
+    
+    useEffect(() => {
+        const currentPath = window.location.pathname;
+        const isManagedBots = currentPath.startsWith("/chat/managedbots");
+        setIsManagedBots(isManagedBots);
+        if (!isManagedBots) {
+            startConversation();
+        }
+    }, []);
+
+    const loadConversation = async () => {
+        if (isManagedBots) {
+            if (!intent || !userId) return;
+        } else {
+            if (!conversationId || !environment || !botId) return;
+        }
+
+        let fetchUrl = '';
+        const queryParams = new URLSearchParams({
+            returnDetailed: "false",
+            returnCurrentStepOnly: "true",
+        });
+
+        if (isManagedBots) {
+            fetchUrl = `${managedBotsBaseUrl}/${intent}/${userId}?${queryParams}`;
+        } else {
+            fetchUrl = `${botsBaseUrl}/${environment}/${botId}/${conversationId}?${queryParams}`;
+        }
+
+        try {
+            const response = await fetch(fetchUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Include any necessary headers, such as Authorization if required
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to load conversation: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            const botReplies: any[] = data.conversationOutputs[0].output || [];
+            botReplies.forEach(reply => {
+                setMessages(currentMessages => [...currentMessages, {sender: 'bot', text: reply.text}]);
+            });
+
+            const botQuickReplies: QuickReply[] = data.conversationOutputs[0].quickReplies || [];
+            setQuickReplies(botQuickReplies);
+
+            setAutoScroll(true);
+        } catch (error) {
+            console.error("Error loading conversation:", error);
+        }
+    };
+
+    useEffect(() => {
+        loadConversation();
+    }, [conversationId, intent])
+
+    const scrollToBottom = () => {
+        if (messagesContainerRef.current) {
+            let scrollHeight = messagesContainerRef.current.scrollHeight;
+            messagesContainerRef.current.scrollTop = scrollHeight;
+
+            // Use setTimeout to give the browser time to render and update scroll positions
+            setTimeout(() => {
+                if (messagesContainerRef.current) {
+                    const scrollTop = messagesContainerRef.current.scrollTop;
+                    const clientHeight = messagesContainerRef.current.clientHeight;
+                    const isAtBottom = scrollHeight - scrollTop - clientHeight <= 5; // tolerance of 5px
+                    setHasNewMessages(!isAtBottom);
+                }
+            }, 100); // Adjust the timeout as needed
+        }
+        setAutoScroll(true);
+    };
+    
+    useEffect(() => {
+        scrollToBottom();
+        setAutoScroll(true);
+    }, [messages]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+    };
+
+    const handleQuickReply = (quickReply: QuickReply) => {
+        sendMessage(quickReply.value);
+        setQuickReplies([]);
+    };
+
+    const sendMessage = async (userInput: string) => {
+        if (!userInput.trim()) return;
+        const userMessage: Message = {sender: 'user', text: userInput};
+        setMessages(currentMessages => [...currentMessages, userMessage]);
+        setIsBotTyping(true);
+
+        const endpoint = isManagedBots ?
+            `${managedBotsBaseUrl}/${intent}/${userId}` :
+            `${botsBaseUrl}/${environment}/${botId}/${conversationId}?userId=${userId}`;
+
+        const method = 'POST';
+        const headers = {'Content-Type': 'application/json'};
+        const body = JSON.stringify({input: userInput});
+
+        try {
+            const response = await fetch(endpoint, {method, headers, body});
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+            const data = await response.json();
+            setIsBotTyping(false);
+
+            if (data.conversationState === 'ERROR') {
+                console.log("ConversationState was ERROR.");
+            }
+
+            const botReplies: any[] = data.conversationOutputs[0].output || [];
+            botReplies.forEach(reply => {
+                setMessages(currentMessages => [...currentMessages, {sender: 'bot', text: reply.text}]);
+            });
+
+            const botQuickReplies: QuickReply[] = data.conversationOutputs[0].quickReplies || [];
+            setQuickReplies(botQuickReplies);
+
+            setAutoScroll(true);
+
+        } catch (error) {
+            console.error("Failed to send message: ", error);
+        }
+    };
+
+    const handleScroll = () => {
+        if (messagesContainerRef.current) {
+            const current = messagesContainerRef.current;
+            const isAtBottom = current.scrollHeight - current.scrollTop - current.clientHeight <= 5; // tolerance of 5px
+
+            if (isAtBottom) {
+                setHasNewMessages(false);
+            } else if (!hasNewMessages) {
+                setHasNewMessages(true);
+            }
+        }
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        sendMessage(input);
+        setInput('');
+    };
+    
+    return (
+        <div>
+            <img id="eddiLogo" className="chatImg" src="/img/logo_eddi.png" alt="EDDI Logo"/>
+            <div className="chat-container">
+                <div className="messages" onScroll={handleScroll} ref={messagesContainerRef}>
+                    {messages.map((msg, index) => (
+                        <div key={index} className={`message ${msg.sender}`}>
+                            {msg.text}
+                        </div>
+                    ))}
+                    {isBotTyping && <div className="loading-indicator">
+                        <img src="/img/loading-indicator.svg" alt="Answer is being generated..."/>
+                    </div>}
+                    <div ref={messagesEndRef}/>
+                </div>
+                {hasNewMessages && (
+                    <button onClick={scrollToBottom} className="scroll-to-bottom">
+                    </button>
+                )}
+                {quickReplies.length > 0 && (
+                    <div className="quick-replies">
+                        {quickReplies.map((quickReply, index) => (
+                            <button key={index} onClick={() => handleQuickReply(quickReply)}>
+                                {quickReply.value}
+                            </button>
+                        ))}
+                    </div>
+                )}
+                <form onSubmit={handleSubmit} className="message-form">
+                    <input
+                        type="text"
+                        value={input}
+                        onChange={handleInputChange}
+                        placeholder="Type a message..."
+                    />
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
--- ui/chat/src/index.js
+++ ui/chat/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Chat from './Chat';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/chat/managedbots/:intent/:userId" element={<Chat />} />
+                <Route path="/chat/:environment/:botId" element={<Chat />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
