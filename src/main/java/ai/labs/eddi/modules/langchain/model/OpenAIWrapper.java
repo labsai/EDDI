26e8e4f44ad1b449382eb678c2fce0c823eedb1a
Gregor Jarisch <gregor@jarisch.net>
2024-05-16 00:30:53 +0200

    cleanup

--- src/main/java/ai/labs/eddi/modules/langchain/model/OpenAIWrapper.java
+++
@@ -1,6 +0,0 @@
-package ai.labs.eddi.modules.langchain.model;
-
-import dev.ai4j.openai4j.OpenAiClient;
-
-public record OpenAIWrapper(LangChainConfiguration langChainConfiguration, OpenAiClient openAiClient) {
-}
