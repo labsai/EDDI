53a53746a662e85a24efb749e4cf2534cdfda973
Gregor Jarisch <gregor@jarisch.net>
2024-05-22 23:59:09 +0200

    Use different implementation for Hugging Face as the one used was broken; added logRequests and logResponses to HuggingFace; adapted in bot father

--- src/main/java/ai/labs/eddi/modules/langchain/impl/builder/HuggingFaceLanguageModelBuilder.java
+++ src/main/java/ai/labs/eddi/modules/langchain/impl/builder/HuggingFaceLanguageModelBuilder.java
@@ -1,9 +1,13 @@
 package ai.labs.eddi.modules.langchain.impl.builder;
 
+import ai.labs.eddi.modules.langchain.impl.LangChainTask;
 import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
+import io.quarkiverse.langchain4j.huggingface.QuarkusHuggingFaceChatModel;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.jboss.logging.Logger;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.Map;
 
@@ -15,16 +19,25 @@ public class HuggingFaceLanguageModelBuilder implements ILanguageModelBuilder {
     private static final String KEY_MODEL_ID = "modelId";
     private static final String KEY_TEMPERATURE = "temperature";
     private static final String KEY_TIMEOUT = "timeout";
+    private static final String KEY_LOG_REQUESTS = "logRequests";
+    private static final String KEY_LOG_RESPONSES = "logResponses";
+    private static final String BASE_URL_HUGGING_FACE = "https://api-inference.huggingface.co/models/";
+
+    private static final Logger LOGGER = Logger.getLogger(LangChainTask.class);
 
     @Override
     public ChatLanguageModel build(Map<String, String> parameters) {
-        var builder = HuggingFaceChatModel.builder();
+        var builder = QuarkusHuggingFaceChatModel.builder();
         if (!isNullOrEmpty(parameters.get(KEY_ACCESS_TOKEN))) {
             builder.accessToken(parameters.get(KEY_ACCESS_TOKEN));
         }
 
         if (!isNullOrEmpty(parameters.get(KEY_MODEL_ID))) {
-            builder.modelId(parameters.get(KEY_MODEL_ID));
+            try {
+                builder.url(URI.create(BASE_URL_HUGGING_FACE + parameters.get(KEY_MODEL_ID)).toURL());
+            } catch (MalformedURLException e) {
+                LOGGER.error("Malformed URL", e);
+            }
         }
 
         if (!isNullOrEmpty(parameters.get(KEY_TIMEOUT))) {
@@ -33,6 +46,14 @@ public class HuggingFaceLanguageModelBuilder implements ILanguageModelBuilder {
 
         if (!isNullOrEmpty(parameters.get(KEY_TEMPERATURE))) {
             builder.temperature(Double.parseDouble(parameters.get(KEY_TEMPERATURE)));
+        }
+
+        if (!isNullOrEmpty(parameters.get(KEY_LOG_REQUESTS))) {
+            builder.logRequests(Boolean.parseBoolean(parameters.get(KEY_LOG_REQUESTS)));
+        }
+
+        if (!isNullOrEmpty(parameters.get(KEY_LOG_RESPONSES))) {
+            builder.logResponses(Boolean.parseBoolean(parameters.get(KEY_LOG_RESPONSES)));
         }
 
         return builder.build();
