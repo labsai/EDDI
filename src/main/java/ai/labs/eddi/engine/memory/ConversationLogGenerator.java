f3d1c6ea12b7b00d1e419175e8cd22de5c6f490e
Gregor Jarisch <gregor@jarisch.net>
2024-06-13 21:54:41 +0200

    Improved ConversationLogGenerator

--- src/main/java/ai/labs/eddi/engine/memory/ConversationLogGenerator.java
+++ src/main/java/ai/labs/eddi/engine/memory/ConversationLogGenerator.java
@@ -28,38 +28,46 @@ public class ConversationLogGenerator {
     public ConversationLog generate() {
         return generate(-1, true);
     }
+
     public ConversationLog generate(int logSize) {
         return generate(logSize, true);
     }
+
     public ConversationLog generate(int logSize, boolean includeFirstBotMessage) {
-        if(conversationMemory == null && memorySnapshot == null) {
+        if (conversationMemory == null && memorySnapshot == null) {
             throw new IllegalStateException("ConversationMemory was null. " +
                     "You need to either set IConversationMemory or ConversationMemorySnapshot");
         }
 
         var conversationLog = new ConversationLog();
-        var conversationOutputs = conversationMemory != null ?
-                conversationMemory.getConversationOutputs() : memorySnapshot.getConversationOutputs();
+        if (logSize != 0) {
+            var conversationOutputs = conversationMemory != null ?
+                    conversationMemory.getConversationOutputs() : memorySnapshot.getConversationOutputs();
 
-        var startIndex = logSize > 0 && conversationOutputs.size() > logSize ? conversationOutputs.size() - logSize : 0;
-        for (var index = startIndex; index < conversationOutputs.size(); index++) {
-            var conversationOutput = conversationOutputs.get(index);
-            var input = conversationOutput.get(OUTPUT_KEY_INPUT, String.class);
-            if (input != null) {
-                conversationLog.getMessages().add(new ConversationLog.ConversationPart(KEY_ROLE_USER, input));
+            var startIndex = 0;
+            if (logSize > 0) {
+                startIndex = conversationOutputs.size() > logSize ? conversationOutputs.size() - logSize : 0;
             }
 
-            var outputList = (List<Map<String, Object>>) conversationOutput.get(OUTPUT_ROLE_OUTPUT);
-            if (outputList != null) {
-                var output = outputList.
-                        stream().toList().stream().
-                        map(item -> item.get(KEY_TEXT).toString()).collect(Collectors.joining(" "));
-                conversationLog.getMessages().add(new ConversationLog.ConversationPart(KEY_ROLE_ASSISTANT, output));
-            }
-        }
+            for (var index = startIndex; index < conversationOutputs.size(); index++) {
+                var conversationOutput = conversationOutputs.get(index);
+                var input = conversationOutput.get(OUTPUT_KEY_INPUT, String.class);
+                if (input != null) {
+                    conversationLog.getMessages().add(new ConversationLog.ConversationPart(KEY_ROLE_USER, input));
+                }
 
-        if(!includeFirstBotMessage && !conversationLog.getMessages().isEmpty()) {
-            conversationLog.getMessages().removeFirst();
+                var outputList = (List<Map<String, Object>>) conversationOutput.get(OUTPUT_ROLE_OUTPUT);
+                if (outputList != null) {
+                    var output = outputList.
+                            stream().toList().stream().
+                            map(item -> item.get(KEY_TEXT).toString()).collect(Collectors.joining(" "));
+                    conversationLog.getMessages().add(new ConversationLog.ConversationPart(KEY_ROLE_ASSISTANT, output));
+                }
+            }
+
+            if (!includeFirstBotMessage && !conversationLog.getMessages().isEmpty()) {
+                conversationLog.getMessages().removeFirst();
+            }
         }
 
         return conversationLog;
