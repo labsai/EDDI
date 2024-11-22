366cb864540b4da40a376b3770d867f6719a0ddb
Gregor Jarisch <gregor@jarisch.net>
2024-11-22 11:41:25 +0100

    Added jlama integration

--- src/main/java/ai/labs/eddi/modules/langchain/bootstrap/LangChainModule.java
+++ src/main/java/ai/labs/eddi/modules/langchain/bootstrap/LangChainModule.java
@@ -27,6 +27,7 @@ public class LangChainModule {
     public static final String LLM_TYPE_GEMINI_VERTEX = "gemini-vertex";
     public static final String LLM_TYPE_GEMINI = "gemini";
     public static final String LLM_TYPE_OLLAMA = "ollama";
+    public static final String LLM_TYPE_JLAMA = "jlama";
 
     private final Map<String, Provider<ILifecycleTask>> lifecycleTaskProviders;
     private final Instance<ILifecycleTask> lifecycleTaskInstance;
@@ -63,9 +64,10 @@ public class LangChainModule {
                 langModelBuilderInstance.select(GeminiLanguageModelBuilder.class).get());
         languageModelApiConnectorBuilders.put(LLM_TYPE_OLLAMA, () ->
                 langModelBuilderInstance.select(OllamaLanguageModelBuilder.class).get());
+        languageModelApiConnectorBuilders.put(LLM_TYPE_JLAMA, () ->
+                langModelBuilderInstance.select(JlamaLanguageModelBuilder.class).get());
 
         lifecycleTaskProviders.put(LangchainTask.ID, () -> lifecycleTaskInstance.select(LangchainTask.class).get());
         LOGGER.debug("Added LangChain Module, current size of lifecycle modules " + lifecycleTaskProviders.size());
     }
-
 }
