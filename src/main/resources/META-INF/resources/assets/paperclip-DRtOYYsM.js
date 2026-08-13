fa892913c2b884edffdd5347d5e5e2e5aabd9a4d
Gregor Jarisch <gregor@jarisch.net>
2026-08-13 22:46:19 +0200

    chore: update Manager UI assets (Manager@e6838b93)

--- src/main/resources/META-INF/resources/assets/paperclip-DRtOYYsM.js
+++
@@ -1,6 +0,0 @@
-import{x as a}from"./index-C1Hb4pcO.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=[["path",{d:"m16 6-8.414 8.586a2 2 0 0 0 2.829 2.829l8.414-8.586a4 4 0 1 0-5.657-5.657l-8.379 8.551a6 6 0 1 0 8.485 8.485l8.379-8.551",key:"1miecu"}]],o=a("paperclip",c);export{o as P};
--- src/main/resources/META-INF/resources/assets/undo-2-BVOtfcvG.js
+++
@@ -1,6 +0,0 @@
-import{x as o}from"./index-C1Hb4pcO.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const a=[["path",{d:"M9 14 4 9l5-5",key:"102s5s"}],["path",{d:"M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H11",key:"f3b9sd"}]],e=o("undo-2",a);export{e as U};
