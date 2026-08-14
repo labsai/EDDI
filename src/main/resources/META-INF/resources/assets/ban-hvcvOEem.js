86f7f06d779f92c4216f347c04a776c391b0748e
Gregor Jarisch <gregor@jarisch.net>
2026-08-14 08:02:05 +0200

    chore: update Manager UI assets (Manager@9c637d0a)

--- src/main/resources/META-INF/resources/assets/ban-hvcvOEem.js
+++
@@ -1,6 +0,0 @@
-import{x as c}from"./index-1GwPeTzd.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M4.929 4.929 19.07 19.071",key:"196cmz"}]],a=c("ban",e);export{a as B};
---
+++ src/main/resources/META-INF/resources/assets/use-debounce-DdvuTTfU.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-CNysKA9P.js";function n(e,t=400){const[r,s]=o.useState(e);return o.useEffect(()=>{const u=setTimeout(()=>s(e),t);return()=>clearTimeout(u)},[e,t]),r}export{n as u};
