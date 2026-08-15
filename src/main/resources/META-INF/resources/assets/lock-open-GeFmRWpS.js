6ab37ca8b80699978487ca2079cc1414c51b83b6
Gregor Jarisch <gregor@jarisch.net>
2026-08-15 10:54:10 +0200

    chore: update Manager UI assets (Manager@b7e67155)

---
+++ src/main/resources/META-INF/resources/assets/lock-open-GeFmRWpS.js
@@ -0,0 +1,6 @@
+import{x as e}from"./index-DKBqK0DX.js";/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 9.9-1",key:"1mm8w8"}]],t=e("lock-open",o);export{t as L};
---
+++ src/main/resources/META-INF/resources/assets/use-persisted-boolean-B0gh7PTO.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-DKBqK0DX.js";function a(t,r){const[e,s]=o.useState(()=>c(t,r));return o.useEffect(()=>{s(c(t,r))},[t,r]),o.useEffect(()=>{try{localStorage.setItem(t,String(e))}catch{}},[t,e]),[e,s]}function c(t,r){try{const e=localStorage.getItem(t);return e!==null?e==="true":r}catch{return r}}export{a as u};
