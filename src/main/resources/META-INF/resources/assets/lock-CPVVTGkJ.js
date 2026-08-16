2a8b902a6a2fc8e0c12e26ebc93030fb7835c02f
Gregor Jarisch <gregor@jarisch.net>
2026-08-16 17:29:43 +0200

    chore: update Manager UI assets (Manager@f5eb188a)

---
+++ src/main/resources/META-INF/resources/assets/lock-CPVVTGkJ.js
@@ -0,0 +1,6 @@
+import{x as e}from"./index-BY6d8wze.js";/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 10 0v4",key:"fwvmzm"}]],t=e("lock",o);export{t as L};
--- src/main/resources/META-INF/resources/assets/trending-up-CXgKIBzo.js
+++
@@ -1,6 +0,0 @@
-import{x as e}from"./index-DSRnXp_G.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M16 7h6v6",key:"box55l"}],["path",{d:"m22 7-8.5 8.5-5-5L2 17",key:"1t1m79"}]],n=e("trending-up",o);export{n as T};
