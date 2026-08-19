3d81a3312a86755f79e80e408afeda84cc55050b
Gregor Jarisch <gregor@jarisch.net>
2026-08-20 00:34:11 +0200

    chore: update Manager UI assets (Manager@bd319d6e)

--- src/main/resources/META-INF/resources/assets/ellipsis-vertical-CPJbsnot.js
+++
@@ -1,6 +0,0 @@
-import{x as c}from"./index-mrsxmuJN.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"12",cy:"5",r:"1",key:"gxeob9"}],["circle",{cx:"12",cy:"19",r:"1",key:"lyex9k"}]],r=c("ellipsis-vertical",e);export{r as E};
--- src/main/resources/META-INF/resources/assets/rotate-cw-CyGEE8in.js
+++
@@ -1,6 +0,0 @@
-import{x as t}from"./index-mrsxmuJN.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8",key:"1p45f6"}],["path",{d:"M21 3v5h-5",key:"1q7to0"}]],a=t("rotate-cw",o);export{a as R};
