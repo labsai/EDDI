0d44c11ba8fff1cf9c77d055ddf7c84d6a505e26
Gregor Jarisch <gregor@jarisch.net>
2026-09-14 16:15:17 +0200

    chore: update Manager UI assets (Manager@21a70982)

---
+++ src/main/resources/META-INF/resources/assets/hand-DcNLcwoQ.js
@@ -0,0 +1,6 @@
+import{x as a}from"./index-DOhDzonq.js";/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M18 11V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2",key:"1fvzgz"}],["path",{d:"M14 10V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v2",key:"1kc0my"}],["path",{d:"M10 10.5V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2v8",key:"10h0bg"}],["path",{d:"M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15",key:"1s1gnw"}]],c=a("hand",e);export{c as H};
